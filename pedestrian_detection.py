e565db26fb8de2a1f306c42ca64267e9ab00602d
Peter Oehme <oehme.pb@gmail.com>
2024-04-19 11:47:45 +0200

    [pedestrian] allow output as command line arg

--- pedestrian_detection.py
+++ pedestrian_detection.py
@@ -2,16 +2,23 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+from argparse import ArgumentParser
 from dmd_segmentation import StreamingDMD, Image
 from logging import basicConfig, INFO
 from matplotlib.animation import ArtistAnimation, PillowWriter
 from tqdm import tqdm
 
 
-START_FRAME = 250 # the data really only starts at frame no 250
+START_FRAME = 250 # the data really only starts at frame no. 250
 
 
 def main():
+    parser = ArgumentParser(
+        prog='PedestrianDetection',
+        description='Detect the movement of pedestrians using Streaming DMD.'
+    )
+    parser.add_argument("--output", choices=["all", "both", "gifs", "plots", "matrices", "none"], default="none")
+    output = parser.parse_args().output
     basicConfig(level=INFO)
 
     im = Image()
@@ -29,12 +36,18 @@ def main():
     data_iter = lambda: zip(X, Y)  # noqa: E731
     hist_lens = [5]
     rrank = 10
-    predictions = []
-    fgs = []
+    results = {
+        "errs": {},
+        "ress": {},
+        "fgs": {},
+        "predictions": {},
+    }
     for hist_len in hist_lens:
         m_str = StreamingDMD(max_rank=rrank, max_hist=hist_len)
         str_errs = 1e-16 * np.ones(x_len - 1)
-        min_res = 1e-16 * np.ones(x_len - 1)
+        str_ress = {}
+        fgs = []
+        predictions = []
         for iter, (x, y) in tqdm(enumerate(data_iter()), total=x_len, desc=f"Streaming QR Memory size {hist_len:2}", ncols=90):
             if iter > 0:
                 ny = np.linalg.norm(y)
@@ -45,7 +58,7 @@ def main():
                 # identify background modes
                 res, modes, amps = m_str.residuals
                 logamps = np.log(amps)
-                min_res[iter - 1] = np.min(np.abs(logamps))
+                str_ress[iter - 1] = res
                 tol = 1e-3
                 bg_indices = list(np.where(np.abs(logamps) < tol)[0])
                 if not bg_indices:
@@ -57,28 +70,32 @@ def main():
 
             m_str.update(x, y)
 
-        print(f"QR Prediction Error Metrics with Memory {hist_len}: max - {np.max(str_errs):6.3e} min - {np.min(str_errs):6.3e} mean - {np.mean(str_errs):6.3e} median - {np.median(str_errs):6.3e}")
-        plt.plot(range(len(str_errs)), str_errs, label=f"Streaming QR with mem {hist_len}")
-        plt.semilogy(min_res, linestyle="none", marker="x")
+        results["errs"][hist_len] = str_errs
+        results["ress"][hist_len] = str_ress
+        results["fgs"][hist_len] = fgs
+        results["predictions"][hist_len] = predictions
 
-    plt.legend(np.repeat(hist_lens, 2))
+    if output == "plots" or output == "both" or output == "all":
+        print("plotting now...")
 
-    plt.show(block=False)
-
-    fig, ax = plt.subplots()
-    ims = []
-    for iter, fg in enumerate(fgs):
-        mat = fg.reshape(im.resolution)
-        mat = np.where(mat > 5e-4, 1, 0)
-        mplot = ax.imshow(mat, cmap=mpl.colormaps["Greys_r"], animated=True)
-        ax.set_title("Foreground Pedestrian")
-        title = ax.text(0, -5, f"DMD Frame {iter}")
-        if iter == 0:
-            ax.imshow(mat, cmap=mpl.colormaps["Greys_r"])
+    if output == "gifs" or output == "both" or output == "all":
+        fig, ax = plt.subplots()
+        ims = []
+        for iter, fg in enumerate(fgs):
+            mat = fg.reshape(im.resolution)
+            mat = np.where(mat > 5e-4, 1, 0)
+            mplot = ax.imshow(mat, cmap=mpl.colormaps["Greys_r"], animated=True)
             ax.set_title("Foreground Pedestrian")
-        ims.append([mplot, title])
-    ani = ArtistAnimation(fig, ims, interval=300)
-    ani.save("pedestrian_foreground.gif", writer=PillowWriter(fps=30))
+            title = ax.text(0, -5, f"DMD Frame {iter}")
+            if iter == 0:
+                ax.imshow(mat, cmap=mpl.colormaps["Greys_r"])
+                ax.set_title("Foreground Pedestrian")
+            ims.append([mplot, title])
+        ani = ArtistAnimation(fig, ims, interval=300)
+        ani.save("pedestrian_foreground.gif", writer=PillowWriter(fps=30))
+
+    if output == "matrices" or output == "all":
+        print("outputing matrices...")
 
 
 if __name__ == "__main__":
